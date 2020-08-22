4a233290aba07162ec44ee5afeca024922bee6d4
sozettaslow <marissa.flores.1992@gmail.com>
2020-08-22 10:58:37 -0700

    initial commit, created button elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
-         <h1>B A N D E R</h1>
+      <h1>B A N D E R</h1>
+      <Button buttonText={"BEE BOOP"} url={"http://www.neopets.com/"}></Button>
+      <Button buttonText={"BEE BOOP"} url={"http://www.neopets.com/"} openInNewTab={true}></Button>
     </div>
   );
 }
