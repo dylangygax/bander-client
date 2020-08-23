a278c69a182485daa90752c34944345289009bd4
sozettaslow <marissa.flores.1992@gmail.com>
2020-08-23 01:31:51 -0700

    minor page adds

--- src/pages/GeneralMessages.js
+++ src/pages/GeneralMessages.js
@@ -1,1 +1,13 @@
 //***THIS IS A STRETCH GOAAAAAAAAAAL */
+
+import React from 'react';
+
+const GeneralMessages = () => {
+    return (
+        <div>
+            <h1>MANY MESSAGEs or maybe not if you're not that skilled sorry bro...</h1>
+        </div>
+    );
+}
+
+export default GeneralMessages;
