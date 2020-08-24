aa815a0576ea87939fb044fa3a1014e4f0530d16
sozettaslow <marissa.flores.1992@gmail.com>
2020-08-24 16:56:37 -0700

    have elementary styling added to login route

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import Button from "../components/Button";
-// import image from ".images/Clef-Note.png";
-
 
 const Login = () => {
     return (
-        <div className="d-flex flex-column">
-            <h1>B A N D E R</h1>
-            <img src={process.env.PUBLIC_URL + "/images/Clef-Note.png"} />
+        <div className="d-flex flex-column m-5 body" >
+            <h1>B A N D E R 1</h1>
+            <img id="logo" src={process.env.PUBLIC_URL + "/images/Clef-Note.png"} />
             <div className="d-flex flex-column">
-                <Button buttonText="Login with Facebook" url={"http://www.neopets.com"} openInNewTab={true} />
-                <Button buttonText="Login with email" url={"http://localhost:3000/app/register"} />
+                <Button buttonText="Login" url={"http://www.neopets.com"} openInNewTab={true} />
+                <Button buttonText="Register" url={"http://localhost:3000/app/register"} />
                 <Button buttonText="Continue without Logging in" url={"http://localhost:3000/app/home"} />
             </div>
-        </div>
+        </div >
     );
 }
 
