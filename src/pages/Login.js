d7340664be1c77587a0b2dccbc4bfae61d135054
sozettaslow <marissa.flores.1992@gmail.com>
2020-08-24 15:20:17 -0700

    created img folder, testing routing to images

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import Button from "../components/Button"
+import Button from "../components/Button";
+// import image from ".images/Clef-Note.png";
+
 
 const Login = () => {
     return (
         <div className="d-flex flex-column">
             <h1>B A N D E R</h1>
+            <img src={process.env.PUBLIC_URL + "/images/Clef-Note.png"} />
             <div className="d-flex flex-column">
                 <Button buttonText="Login with Facebook" url={"http://www.neopets.com"} openInNewTab={true} />
                 <Button buttonText="Login with email" url={"http://localhost:3000/app/register"} />
