3d847d8b4fe5059be84c2184cc767152eb26caa6
DylanGygax <dylangygax@ncf.edu>
2020-08-28 19:11:13 -0500

    successfully rendered a specific user to the home.js page

--- src/models/user.js
+++ src/models/user.js
@@ -3,6 +3,8 @@ const REACT_APP_API_URL = /*deployed url || */"http://localhost:4000/api/v1"
 export default class UserModel {
     //for showing data from one user
     static show(userID) {
+        console.log('in show')
+        console.log(userID)
         return fetch(`${REACT_APP_API_URL}/users/${userID}`).then(res => res.json())
     }
 
