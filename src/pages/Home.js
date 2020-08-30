6871bcf14627402b50c1b256c9c938bfc6302d94
DylanGygax <dylangygax@ncf.edu>
2020-08-30 11:52:52 -0500

    Home now displays first user in the queue

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,25 +6,25 @@ import {QueueContext, QueueContextProvider} from "../QueueContext"
 
 const Home = (props) => {
     //temporary hardcoding
-    const [id, setId] = useState('5f4afbcbd976b21a3f29b8d6')
-    console.log(id)
+    // const [id, setId] = useState('5f4afbcbd976b21a3f29b8d6')
+    // console.log(id)
     const [match, setMatch] = useState(null)
     //actual queue version
     const [queue, setQueue] = useContext(QueueContext)
     console.log(queue)
     useEffect(() => {
         console.log(queue)
-        UserModel.show(id)
+        UserModel.show(queue[0][0]) //ID of the first user in queue. queue elements are of form: [id, distance]
             .then(data => {
-                console.log(id)
+                console.log(queue[0])
                 console.log(data.user)
                 setMatch(data.user)
             })
-    }, [id])
+    }, [queue])
 
     return (
         <div className="body">
-            {match
+            {match 
             ? <>
                 <MatchCard {...match}/>
                 <Footer matchId={match._id}/>
