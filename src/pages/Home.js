47688300bd3278c0fb91d042f7a96e8db2c47426
DylanGygax <dylangygax@ncf.edu>
2020-08-30 11:32:05 -0500

    queue context created and being read by Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import MatchCard from "../components/MatchCard";
 import Footer from "../components/Footer";
 import UserModel from '../models/user';
+import {QueueContext, QueueContextProvider} from "../QueueContext"
 
 const Home = (props) => {
+    //temporary hardcoding
     const [id, setId] = useState('5f4afbcbd976b21a3f29b8d6')
     console.log(id)
     const [match, setMatch] = useState(null)
+    //actual queue version
+    const [queue, setQueue] = useContext(QueueContext)
+    console.log(queue)
     useEffect(() => {
+        console.log(queue)
         UserModel.show(id)
             .then(data => {
                 console.log(id)
